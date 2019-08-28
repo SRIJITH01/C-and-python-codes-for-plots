dc25b31cef751d89a8e53f221f5fb01a038a37c0
SRIJITH01 <54314892+SRIJITH01@users.noreply.github.com>
2019-08-28 04:55:16 +0000

    Create presentation2.py

---
+++ presentation2.py
@@ -0,0 +1,54 @@
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+from funcs import *
+import numpy as np
+
+#defining lines : x(k) = A + k*l
+O = np.array([0,0,0]).reshape((3,1))
+l1 = np.array([1,0,0]).reshape((3,1))
+O = np.array([0,0,0]).reshape((3,1))
+l2 = np.array([np.sqrt(3)/2,0,1/2]).reshape((3,1))
+
+
+
+
+
+#creating x,y for 3D plotting
+len = 1
+xx, yy = np.meshgrid(range(len), range(len))
+#setting up plot
+fig = plt.figure()
+ax = fig.add_subplot(111,projection='3d',aspect='equal')
+
+
+O = np.array([0,0,0]).reshape((3,1))
+l3= np.array([-np.sqrt(3)/2,1/2,0]).reshape((3,1))
+A = np.array([1,0,0]).reshape((3,1))
+B = np.array([-np.sqrt(3)/2,1/2,0]).reshape((3,1))
+C = np.array([np.sqrt(3)/2,0,1/2]).reshape((3,1))
+
+l1_p = line_dir_pt(l1,O)
+l2_p = line_dir_pt(l2,O)
+l3_p = line_dir_pt(l3,O)
+z=0
+ax.scatter(O[0],O[1],O[2],'o',label="Point O")
+ax.scatter(A[0],A[1],A[2],'o',label="Point A")
+ax.scatter(C[0],C[1],C[2],'o',label="Point C")
+ax.scatter(B[0],B[1],B[2],'o',label="Point B")
+ax.plot_surface(xx, yy, z, color='r',alpha=1)
+
+#plotting line
+plt.plot(l1_p[0,:],l1_p[1,:],l1_p[2,:],label="Line L1")
+plt.plot(l2_p[0,:],l2_p[1,:],l2_p[2,:],label="Line L2")
+plt.plot(l3_p[0,:],l3_p[1,:],l3_p[2,:],label="Line L3")
+
+#corresponding z for planes
+
+
+#plotting planes
+
+#show plot
+plt.xlabel('$x$');plt.ylabel('$y$')
+plt.legend(loc='best');plt.grid()
+plt.axis("equal")
+plt.show()
